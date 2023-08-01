ec9d6a9a7cd08004ec86ad9f42a9afb66c511142
FlorianBetin <florian.betin.pro@gmail.com>
2023-08-01 14:47:41 +0200

    Q2

---
+++ pokedex_react_Q1/src/components/Pokemoncards.jsx
@@ -0,0 +1,15 @@
+import '../styles/pokemoncards.css';
+
+const PokemonCards = () => {
+return (
+    
+    <figure className="pokemonCardFigure">
+        <img className="pokemonPic" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Image de Bulbizarre" />
+        <figcaption>
+        Bulbizarre
+        </figcaption>
+    </figure>
+)
+}
+
+export default PokemonCards
